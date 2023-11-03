4854826c18dbc9c48038b2bbc6caff5803323bdd
Bryan Samek <bsamek@hawk.iit.edu>
2023-11-02 20:22:17 -0500

    Trying to make mobile friendly
    Added values to make the screen accessible on smaller devices, specifically the main sign up portion and image.

--- js/site.js
+++ js/site.js
@@ -2,7 +2,14 @@
 jQuery(function($) {
   $('html').removeClass('nojs');
   $('html').addClass('hasjs');
+
+  document.addEventListener('DOMContentLoaded',function () {
+	  const form= document.querySelector('form');
+	  
+  form.addEventListener('submit' function (event) {
+	  event.preventDefault();
+	  alert('Thanks for Signing Up! See you at the Elbertsons soon!);
   let form= document.querySelector("form");
   form.addEventListener("submit", () => {
 	  form.classList.add("signed-up");
-});
+  })};

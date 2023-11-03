f0d4cdc795fd614fa8ca91497f5ea186b975e618
Bryan Samek <bsamek@hawk.iit.edu>
2023-11-02 21:13:05 -0500

    Added slight style & tinkering
    Js has a specific error, trying to identify where it appears, testing to make the dimensions slightly better on mobile phones as the colors go off the screen or get cut in half.

--- js/site.js
+++ js/site.js
@@ -1,13 +1,9 @@
 // jQuery 3.x-style ready event and locally scoped $
-jQuery(function($) {
-  $('html').removeClass('nojs');
-  $('html').addClass('hasjs');
-
-  document.addEventListener('DOMContentLoaded', function () {
-	  const form= document.querySelector('form');
+document.addEventListener('DOMContentLoaded', function () {
+  const form= document.querySelector('form');
 	  
-  form.addEventListener('submit' function (event) {
-	  event.preventDefault();
-	  alert('Thanks for Signing Up! See you at the Elbertsons soon!');
+form.addEventListener('submit' function (event) {
+  event.preventDefault();
+  alert('Thanks for Signing Up! See you at the Elbertsons soon!');
   });
-  });
+ });

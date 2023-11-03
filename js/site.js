ac077c44763afbd3fb261b328e8d8379b04a5a1d
Bryan Samek <bsamek@hawk.iit.edu>
2023-11-02 20:53:51 -0500

    Fixing error with js
    Did not close the args, hopefully submit will now appear correctly upon filling out queries

--- js/site.js
+++ js/site.js
@@ -3,11 +3,11 @@ jQuery(function($) {
   $('html').removeClass('nojs');
   $('html').addClass('hasjs');
 
-  document.addEventListener('DOMContentLoaded',function () {
+  document.addEventListener('DOMContentLoaded', function () {
 	  const form= document.querySelector('form');
 	  
   form.addEventListener('submit' function (event) {
 	  event.preventDefault();
 	  alert('Thanks for Signing Up! See you at the Elbertsons soon!');
 
- });
+  }});
